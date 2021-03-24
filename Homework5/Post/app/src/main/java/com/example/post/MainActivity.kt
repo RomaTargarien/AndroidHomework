9d63605c6b251bb354ffcc1a5c3920fac7d8811b
Roma Tolstikov <tolstikovski@mai.ru>
2021-03-24 18:50:40 +0300

    post add commit

--- Homework5/Post/app/src/main/java/com/example/post/MainActivity.kt
+++ Homework5/Post/app/src/main/java/com/example/post/MainActivity.kt
@@ -1,17 +1,26 @@
 package com.example.post
 
+import android.content.Context
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.widget.Toolbar
+import androidx.annotation.RequiresApi
+import androidx.core.view.WindowCompat
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.post.adapters.PostAdapter
 import com.example.post.utils.PostS_LIST
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.internal.NavigationMenu
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var recyclerView: RecyclerView
     private lateinit var bottomNavigationView: BottomNavigationView
+    private lateinit var toolbar: androidx.appcompat.widget.Toolbar
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,5 +35,11 @@ class MainActivity : AppCompatActivity() {
         bottomNavigationView = findViewById(R.id.bottomNavigationView)
         bottomNavigationView.background = null
 
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu,menu)
+        return true
     }
 }
